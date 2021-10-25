4e28730a4135d7542cf4e6a00b8566aa52b2fe6e
Eric Yi <erickanginyi@gmail.com>
2021-10-25 17:02:23 -0400

    check this out

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react"
 
 function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:9292")
+      .then(resp => resp.json())
+      .then(data => setData(data))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.message}
     </div>
   );
 }
