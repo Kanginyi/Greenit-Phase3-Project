75e7b35b1d45691599a99b0f52cda2af9bcc7e0d
Eric Yi <erickanginyi@gmail.com>
2021-10-27 16:56:32 -0400

    Greenit Logo

--- src/Components/Header/NavBar.jsx
+++ src/Components/Header/NavBar.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import SearchBar from './SearchBar'
 import User from './User'
 
+import logo from "../../reddit-logo.png"
+
 const NavBar = () => {
     return (
         <>
             <div className='nav'>
                 <span className='logo-box'>
+                    <img src={logo} alt="Greenit Logo" title="Greenit >:^(" width="50px" height="50px"/>
                     <h4>Greenit</h4>
                 </span>     
                 <SearchBar />  
